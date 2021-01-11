0189074ee28ef316d9981add05b4c3c56622e0f9
tristanponessa <tristanponessa@hotmail.com>
2021-01-11 16:22:23 +0100

    +

--- session.py
+++ session.py
@@ -26,3 +26,10 @@ def updating_session():
         session['cart_item'] = cart_item
 
     return res
+
+
+# Using a production configuration
+app.config.from_object('config.ProdConfig')
+
+# Using a development configuration
+app.config.from_object('config.DevConfig')
